8d7ea5fc4de9a4772dfead18035b6cb44a6480b8
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-01-26 07:58:00 +0700

    first

---
+++ Test_1/src/baiTest_1.java
@@ -0,0 +1,12 @@
+public class baiTest_1 {
+    public static void main(String[] args) {
+        System.out.println("Dong 1");//sout
+        System.out.println("Dong 2");//sout
+        System.out.println("Dong 3");//sout
+        System.out.println("");
+        //t
+        System.out.println("\tAnh thich em");
+        //n
+        System.out.println("Anh thich em\n nhung khoan yeu");
+    }
+}
