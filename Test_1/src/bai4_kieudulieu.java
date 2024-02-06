9197f163ba2efed6ea7abcfa779fad357fe7331d
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-02-06 20:17:44 +0700

    update

---
+++ Test_1/src/bai4_kieudulieu.java
@@ -0,0 +1,11 @@
+public class bai4_kieudulieu {
+    public static void main(String[] args) {
+        //kiem tra kieu nguyen
+        System.out.println("max cu kieu int " +Integer.MAX_VALUE);
+        System.out.println("min cu kieu int " +Integer.MIN_VALUE);
+
+        //kiem tra kieu byte
+        System.out.println("max cu kieu byte " +Byte.MAX_VALUE);
+        System.out.println("min cu kieu byte " +Byte.MIN_VALUE);
+    }
+}
---
+++ Test_1/src/bai5_khaibaobienjava.java
@@ -0,0 +1,24 @@
+public class bai5_khaibaobienjava {
+    public static void main(String[] args) {
+        //khai bao bien
+        int a;
+        byte tuoi;
+        float diemtoan;
+        //khai bao nhieu bien mot luc
+        int b,c,d,e;
+
+        //khoi tao bien
+        int tuoiCon = 10;
+        float diemVan = 7.5f;
+        double diemAnh = 7.5;
+
+        System.out.println("tuoi con\t" + tuoiCon);
+        tuoiCon = 15;
+        System.out.println("tuoi con sau khi thay doi\t" + tuoiCon);
+
+
+        // hang so
+        final int DOSOI = 100;
+        
+    }
+}
